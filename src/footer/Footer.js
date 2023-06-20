f79916d5e02b8bb5d2cb4d1a1a8f18426b2c532e
Allochka11 <allavelychko11@gmail.com>
2023-06-20 14:27:03 +0300

    2 lesson adding scss

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -1,6 +1,12 @@
 import React from "react";
 import s from './Footer.module.css';
 import styleContainer from '../common/styles/Container.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome' ;
+import {faReact, faTelegram} from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import {faPhone} from "@fortawesome/free-solid-svg-icons/faPhone";
+import {faViber} from "@fortawesome/free-brands-svg-icons/faViber";
+import {faLinkedin} from "@fortawesome/free-brands-svg-icons/faLinkedin";
 
 
 export const Footer = (props) => {
@@ -10,12 +16,23 @@ export const Footer = (props) => {
             <div className={`${styleContainer.container} ${s.footerContainer} `}>
                 <h2 className={s.title}>Alla Velychko</h2>
                 <div className={s.footerIcons}>
-                    <div className={s.icon}></div>
-                    <div className={s.icon}></div>
-                    <div className={s.icon}></div>
-                    <div className={s.icon}></div>
+                    <a href="sms:+380951529370"><FontAwesomeIcon className={s.icon} icon={faPhone} /></a>
+                    <a href="tel:+380951529370">
+                        <FontAwesomeIcon className={s.icon} icon={faTelegram} />
+                    </a>
+                    <a href="viber://chat?number=+380951529370">
+                        <FontAwesomeIcon className={s.icon} icon={faViber} />
+                    </a>
+                    <a href="https://www.linkedin.com/in/alla-velychko/" target="_blank">
+                        <FontAwesomeIcon className={s.icon} icon={faLinkedin} />
+                    </a>
+
+                    <a href="mailto:allavelychko11@gmail.com">
+                        <FontAwesomeIcon className={s.icon} icon={faEnvelope}/>
+                    </a>
+
                 </div>
-                <span className={s.copyright}>© 2022 All rights reserved</span>
+                <span className={s.copyright}>© 2023  All rights reserved</span>
             </div>
         </div>
     );
